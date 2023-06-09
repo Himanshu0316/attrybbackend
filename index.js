a1bb812b0492ecc53558422bd84777c1a5d82c79
Himanshu0316 <hsupadhyay316@gmail.com>
2023-06-09 17:57:35 +0530

    h

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const { MarketInventoryModel } = require("./model/Marketinventory.model")
 
 const app=express()
 const corsOptions ={
-    origin:'http://localhost:3000', 
     credentials:true,
-    optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
 app.use(express.json())
